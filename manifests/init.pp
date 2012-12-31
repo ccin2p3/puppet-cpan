2e1c844b164c740c18dd9036335732784beb5524
Sebastian Geidies <geidies@gmail.com>
2012-12-31 16:24:22 +0100

    cpan Config.pm correct location for RH

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,26 +2,33 @@ class cpan {
   case $operatingsystem {
     debian,ubuntu : {
       package { "perl-modules": ensure => installed }
+      file { [ "/etc/perl", "/etc/perl/CPAN" ]:
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => 0755,
+      }
+
+      file { "/etc/perl/CPAN/Config.pm":
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => 644,
+        source => "puppet:///cpan/Config.pm",
+        require => File["/etc/perl/CPAN"]
+      }
     }
     centos,redhat : {
-      # nothing to do.
+      file { "/usr/lib/perl5/5.8.8/CPAN/Config.pm":
+        ensure => present,
+        owner  => root,
+        group  => root,
+        mode   => 644,
+        source => "puppet:///cpan/Config.pm",
+        require => File["/etc/perl/CPAN"]
+      }
     }
   }
 
-  file { [ "/etc/perl", "/etc/perl/CPAN" ]:
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 0755,
-  }
-
-  file { "/etc/perl/CPAN/Config.pm":
-    ensure => present,
-    owner  => root,
-    group  => root,
-    mode   => 644,
-    source => "puppet:///cpan/Config.pm",
-    require => File["/etc/perl/CPAN"]
-  }
 
 }
