4ba91eadbbc7815a1be1ccfba79f686548707ba7
Sebastian Geidies <geidies@gmail.com>
2012-12-31 15:32:34 +0100

    make sure path exists before putting file in it.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,13 @@ class cpan {
     ensure => installed,
   }
 
+  file { [ "/etc/perl", "/etc/perl/CPAN" ]:
+    ensure => directory,
+    owner => root,
+    group => root,
+    mode => 0755,
+  }
+
   file { "/etc/perl/CPAN/Config.pm":
     ensure => present,
     owner  => root,
