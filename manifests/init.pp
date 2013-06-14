4bd557876c34a625a7dc76cb4df5d52445c33a7f
Vilmos Nebehaj <vilmos.nebehaj@meltwater.com>
2013-06-14 11:41:40 +0200

    Add CentOS6 support.

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,12 +19,22 @@ class cpan {
       }
     }
     centos,redhat : {
-      file { "/usr/lib/perl5/5.8.8/CPAN/Config.pm":
-        ensure => present,
-        owner  => root,
-        group  => root,
-        mode   => 644,
-        source => "puppet:///modules/cpan/Config.pm",
+      if versioncmp($::operatingsystemrelease, '6.0') >= 0 {
+        file { "/usr/share/perl5/CPAN/Config.pm":
+          ensure => present,
+          owner  => root,
+          group  => root,
+          mode   => 644,
+          source => "puppet:///modules/cpan/Config.pm",
+        }
+      } else {
+        file { "/usr/lib/perl5/5.8.8/CPAN/Config.pm":
+          ensure => present,
+          owner  => root,
+          group  => root,
+          mode   => 644,
+          source => "puppet:///modules/cpan/Config.pm",
+        }
       }
     }
   }
