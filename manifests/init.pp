848dd93ac3bf503f21a1a5cd99df7f6eea99795e
William Leese <william.leese@meltwater.com>
2013-07-18 10:02:28 +0900

    - install perl-CPAN as a dependency - puppetlint fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,43 +1,45 @@
-class cpan {
+# Class cpan
+class cpan () {
   case $::operatingsystem {
-    debian,ubuntu : {
-      package { "perl-modules": ensure => installed }
-      file { [ "/etc/perl", "/etc/perl/CPAN" ]:
+    debian,ubuntu: {
+      package { 'perl-modules': ensure => installed }
+      file { [ '/etc/perl', '/etc/perl/CPAN' ]:
         ensure => directory,
-        owner => root,
-        group => root,
-        mode => 0755,
-      }
-
-      file { "/etc/perl/CPAN/Config.pm":
-        ensure => present,
         owner  => root,
         group  => root,
-        mode   => 644,
-        source => "puppet:///modules/cpan/Config.pm",
-        require => File["/etc/perl/CPAN"]
+        mode   => '0755',
+      }
+      file { '/etc/perl/CPAN/Config.pm':
+        ensure  => present,
+        owner   => root,
+        group   => root,
+        mode    => '0644',
+        source  => 'puppet:///modules/cpan/Config.pm',
+        require => File['/etc/perl/CPAN']
       }
     }
-    centos,redhat : {
-      if versioncmp($::operatingsystemrelease, '6.0') >= 0 {
-        file { "/usr/share/perl5/CPAN/Config.pm":
+    centos,redhat: {
+      package { 'perl-CPAN': ensure => installed }
+      if versioncmp($::operatingsystemmajrelease, '6') >= 0 {
+        file { '/usr/share/perl5/CPAN/Config.pm':
           ensure => present,
           owner  => root,
           group  => root,
-          mode   => 644,
-          source => "puppet:///modules/cpan/Config.pm",
+          mode   => '0644',
+          source => 'puppet:///modules/cpan/Config.pm',
         }
       } else {
-        file { "/usr/lib/perl5/5.8.8/CPAN/Config.pm":
+        file { '/usr/lib/perl5/5.8.8/CPAN/Config.pm':
           ensure => present,
           owner  => root,
           group  => root,
-          mode   => 644,
-          source => "puppet:///modules/cpan/Config.pm",
+          mode   => '0644',
+          source => 'puppet:///modules/cpan/Config.pm',
         }
       }
     }
+    default: {
+      fail("Module ${module_name} is not supported on ${::operatingsystem}")
+    }
   }
-
-
 }
