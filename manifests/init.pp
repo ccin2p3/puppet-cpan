215ec5dca377b709ec7bf04f62b409026ecd2344
William Leese <william.leese@meltwater.com>
2014-01-29 13:28:58 +0900

    make more flexbile

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,40 +1,53 @@
 # Class cpan
-class cpan () {
+class cpan (
+  $manage_config = true,
+  $manage_package = true,
+) {
   case $::operatingsystem {
     debian,ubuntu: {
-      package { 'perl-modules': ensure => installed }
-      file { [ '/etc/perl', '/etc/perl/CPAN' ]:
-        ensure => directory,
-        owner  => root,
-        group  => root,
-        mode   => '0755',
+      if $manage_package {
+        package { 'perl-modules': ensure => installed }
       }
-      file { '/etc/perl/CPAN/Config.pm':
-        ensure  => present,
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        source  => 'puppet:///modules/cpan/Config.pm',
-        require => File['/etc/perl/CPAN']
+      if $manage_config {
+        file { [ '/etc/perl', '/etc/perl/CPAN' ]:
+          ensure => directory,
+          owner  => root,
+          group  => root,
+          mode   => '0755',
+        }
+        file { '/etc/perl/CPAN/Config.pm':
+          ensure  => present,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          source  => 'puppet:///modules/cpan/Config.pm',
+          require => File['/etc/perl/CPAN']
+        }
       }
     }
     centos,redhat: {
       if versioncmp($::operatingsystemmajrelease, '6') >= 0 {
-        package { 'perl-CPAN': ensure => installed }
-        file { '/usr/share/perl5/CPAN/Config.pm':
-          ensure => present,
-          owner  => root,
-          group  => root,
-          mode   => '0644',
-          source => 'puppet:///modules/cpan/Config.pm',
+        if $manage_package {
+          package { 'perl-CPAN': ensure => installed }
+        }
+        if $manage_config {
+          file { '/usr/share/perl5/CPAN/Config.pm':
+            ensure => present,
+            owner  => root,
+            group  => root,
+            mode   => '0644',
+            source => 'puppet:///modules/cpan/Config.pm',
+          }
         }
       } else {
-        file { '/usr/lib/perl5/5.8.8/CPAN/Config.pm':
-          ensure => present,
-          owner  => root,
-          group  => root,
-          mode   => '0644',
-          source => 'puppet:///modules/cpan/Config.pm',
+        if $manage_config {
+          file { '/usr/lib/perl5/5.8.8/CPAN/Config.pm':
+            ensure => present,
+            owner  => root,
+            group  => root,
+            mode   => '0644',
+            source => 'puppet:///modules/cpan/Config.pm',
+          }
         }
       }
     }
