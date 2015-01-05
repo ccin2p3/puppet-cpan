0edd49a202991cec02a462edfd5b2748142f36a6
Sebastian Geidies <geidies@gmail.com>
2015-01-05 11:17:16 +0100

    fix indentation.

--- lib/puppet/provider/cpan/default.rb
+++ lib/puppet/provider/cpan/default.rb
@@ -13,16 +13,16 @@ Puppet::Type.type(:cpan).provide( :default ) do
   end
 
   def latest?
-  if resource[:local_lib]
-    ll = "-Mlocal::lib=#{resource[:local_lib]}"
-  end
-	current_version=`perl #{ll} -M#{resource[:name]} -e 'print $#{resource[:name]}::VERSION'`
-	cpan_str=`perl #{ll} -e 'use CPAN; my $mod=CPAN::Shell->expand("Module","#{resource[:name]}"); printf("%s", $mod->cpan_version eq "undef" || !defined($mod->cpan_version) ? "-" : $mod->cpan_version);'`
-	latest_version=cpan_str.match(/^[0-9]+.?[0-9]*$/)[0]
-	current_version.chomp
-	latest_version.chomp
-	if current_version < latest_version
-	return false else return true end
+    if resource[:local_lib]
+      ll = "-Mlocal::lib=#{resource[:local_lib]}"
+    end
+    current_version=`perl #{ll} -M#{resource[:name]} -e 'print $#{resource[:name]}::VERSION'`
+    cpan_str=`perl #{ll} -e 'use CPAN; my $mod=CPAN::Shell->expand("Module","#{resource[:name]}"); printf("%s", $mod->cpan_version eq "undef" || !defined($mod->cpan_version) ? "-" : $mod->cpan_version);'`
+    latest_version=cpan_str.match(/^[0-9]+.?[0-9]*$/)[0]
+    current_version.chomp
+    latest_version.chomp
+    if current_version < latest_version
+    return false else return true end
   end
 
   def create
@@ -33,10 +33,10 @@ Puppet::Type.type(:cpan).provide( :default ) do
 
     Puppet.debug("cpan #{resource[:name]}")
     if resource[:force] == false then
-    	system("yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
+      system("yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
     else
-	Puppet.info("Forcing install for #{resource[:name]}")
-	system("yes | perl #{ll} -MCPAN -e 'CPAN::force CPAN::install #{resource[:name]}'")
+      Puppet.info("Forcing install for #{resource[:name]}")
+      system("yes | perl #{ll} -MCPAN -e 'CPAN::force CPAN::install #{resource[:name]}'")
     end
 
     #cpan doesn't always provide the right exit code, so we double check
@@ -52,22 +52,22 @@ Puppet::Type.type(:cpan).provide( :default ) do
   end
   
   def update
-	Puppet.info("Upgrading cpan module #{resource[:name]}")
-	Puppet.debug("cpan #{resource[:name]}")
-  if resource[:local_lib]
-    ll = "-Mlocal::lib=#{resource[:local_lib]}"
-  end
-	if resource[:force] == false then
-    		system("yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
-	else
-		Puppet.info("Forcing upgrade for #{resource[:name]}")
-		system("yes | perl #{ll} -MCPAN -e 'CPAN::force CPAN::install #{resource[:name]}'")
-	end
-	estatus = $?.exitstatus
-	
-	if estatus != 0
-      	   raise Puppet::Error, "CPAN::install #{resource[:name]} failed with error code #{estatus}"
-    	end
+    Puppet.info("Upgrading cpan module #{resource[:name]}")
+    Puppet.debug("cpan #{resource[:name]}")
+    if resource[:local_lib]
+      ll = "-Mlocal::lib=#{resource[:local_lib]}"
+    end
+    if resource[:force] == false then
+          system("yes | perl #{ll} -MCPAN -e 'CPAN::install #{resource[:name]}'")
+    else
+      Puppet.info("Forcing upgrade for #{resource[:name]}")
+      system("yes | perl #{ll} -MCPAN -e 'CPAN::force CPAN::install #{resource[:name]}'")
+    end
+    estatus = $?.exitstatus
+    
+    if estatus != 0
+      raise Puppet::Error, "CPAN::install #{resource[:name]} failed with error code #{estatus}"
+    end
   end
 
   def exists?
