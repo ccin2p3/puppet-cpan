e690c6fbdae28c4750b6676eff2ad9e8d0260707
Jens Lehme <jens.lehme@magicinternet.de>
2015-06-09 15:27:01 +0200

    Refactoring, make puppet 4 conform, add some tests

---
+++ manifests/config.pp
@@ -0,0 +1,46 @@
+#
+class cpan::config inherits cpan {
+  if $cpan::manage_config {
+    case $::osfamily {
+      'Debian': {
+        file { [ '/etc/perl', '/etc/perl/CPAN' ]:
+          ensure => directory,
+          owner  => root,
+          group  => root,
+          mode   => '0755',
+        }
+        file { '/etc/perl/CPAN/Config.pm':
+          ensure  => present,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          content => template($cpan::config_template),
+          require => File['/etc/perl/CPAN'],
+        }
+      }
+      'RedHat': {
+        if versioncmp($::operatingsystemmajrelease, '6') >= 0 {
+          file { '/usr/share/perl5/CPAN/Config.pm':
+            ensure  => present,
+            owner   => root,
+            group   => root,
+            mode    => '0644',
+            content => template($cpan::config_template),
+          }
+        } else {
+          file { '/usr/lib/perl5/5.8.8/CPAN/Config.pm':
+            ensure  => present,
+            owner   => root,
+            group   => root,
+            mode    => '0644',
+            source  => 'puppet:///modules/cpan/Config.pm',
+            content => template($cpan::config_template),
+          }
+        }
+      }
+      default: {
+        fail("Module ${module_name} is not supported on ${::osfamily} os.")
+      }
+    }
+  }
+}
